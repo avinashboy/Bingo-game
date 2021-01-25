2287d20945adb3cd093a621d41f5ebaca0874ff2
avinashboy <52669990+avinashboy@users.noreply.github.com>
2021-01-25 19:37:27 +0530

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 // socket connect
-const socket = io.connect("http://localhost:3000")
+const socket = io.connect(url)
 
 // gobal
 let numberArr = [], martixArr = [], dummyList = []
@@ -18,7 +18,8 @@ Done by Nivedha
 Follow github-- > https: //github.com/nibfic99
 */
 
-let niv = 5, f = 0, l, k, s, sum, num, wi = 0, li = 0, ne = 0, countStrike=0
+let niv = 5, f = 0, l, k, s, sum, num, wi = 0, li = 0, ne = 0, countStrike = 0
+// row and coloum
 let n = m = 5
 
 // room name
@@ -35,30 +36,34 @@ const playerName = prompt("Name")
 
 
 
+socket.on("goto-main-page", () => { window.location = url; })
 
 
-  socket.emit("user-name", {
-    name: playerName,
-    room: x
-  })
+socket.emit("user-name", {
+  name: playerName,
+  room: x
+})
 
-  socket.on("list_of_user", data => {
-    byServer = data.number
-    if (data.clients.length === data.max) runOnetime(), showName(data.clients), init()
-    
-  })
+socket.on("list_of_user", data => {
+  byServer = data.number
+  if (data.clients.length === data.max) runOnetime(), showName(data.clients), init()
 
-  $('#playName').text(`${playerName}`)
+})
+
+$('#playName').text(`${playerName}`)
 
 socket.on("swin", data => {
-  $('#show').append(`<li>${data} he got bingo..!</li>`)
-  })
+  $('#show').append(`<li><h2>You lose the game<h2></li>`)
+})
 
 socket.on("number", data => {
-    checkStrike(data.number)
-    serverName = data.name
-    showName(data.clients)
-  })
+  if (data.clients.length !== 0 || data.clients !== null) {
+    return checkStrike(data.number),
+      serverName = data.name,
+      showName(data.clients)
+  }
+
+})
 
 // function
 function shuffle(a) {
@@ -83,7 +88,7 @@ function init() {
   }, 1000);
 }
 
-function shuffleOneTime(){
+function shuffleOneTime() {
   let shu = shuffle(numberArr)
   while (shu.length) {
     martixArr.push(shu.splice(0, 5));
@@ -124,120 +129,118 @@ function getNumber(e) {
       room: x,
       altName: tempName
     })
-    
+
   }
-  
+
 }
 
 
+// to strike bingo char
 function checkMe() {
-  if (countStrike === 1) {
-    document.getElementById('iob').classList.add("done_color")
-  }
-  if (countStrike === 2) {
-    document.getElementById('ioi').classList.add("done_color")
-  }
-  if (countStrike === 3) {
-    document.getElementById('ion').classList.add("done_color")
-  }
-  if (countStrike === 4) {
-    document.getElementById('iog').classList.add("done_color")
-  }
-  if (countStrike === 5) {
-    document.getElementById('ioo').classList.add("done_color")
-  }
+  if (countStrike === 1) return document.getElementById('iob').classList.add("done_color")
+
+  if (countStrike === 2) return document.getElementById('ioi').classList.add("done_color")
+
+  if (countStrike === 3) return document.getElementById('ion').classList.add("done_color")
+
+  if (countStrike === 4) return document.getElementById('iog').classList.add("done_color")
+
+  if (countStrike === 5) return document.getElementById('ioo').classList.add("done_color")
+
 }
 
-
+// check strike
 function checkStrike(number) {
-    $(`#${number}`).addClass('already_press')
-    for (let i = 0; i < martixArr.length; i++) {
-      for (let j = 0; j < martixArr[i].length; j++) {
-        if (number === martixArr[i][j]) {
-          martixArr[i][j] = 0
-          l = i;
-          k = j;
-          ne = 1;
-          break;
-        }
-      }
-      if (ne == 1) {
+  $(`#${number}`).addClass('already_press')
+  for (let i = 0; i < martixArr.length; i++) {
+    for (let j = 0; j < martixArr[i].length; j++) {
+      if (number === martixArr[i][j]) {
+        martixArr[i][j] = 0
+        l = i;
+        k = j;
+        ne = 1;
         break;
       }
     }
-    ne = 0;
-    for (s = 0; s < m; s++) {
-      sum = sum + martixArr[l][s];
+    if (ne == 1) {
+      break;
     }
-    if (sum == 0) {
-      countStrike += 1
-      checkMe()
-      niv = niv - 1;
-    }
-    sum = 0;
+  }
+  ne = 0;
+  for (s = 0; s < m; s++) {
+    sum = sum + martixArr[l][s];
+  }
+  if (sum == 0) {
+    countStrike += 1
+    checkMe()
+    niv = niv - 1;
+  }
+  sum = 0;
+  for (s = 0; s < n; s++) {
+    sum = sum + martixArr[s][k];
+  }
+  if (sum == 0) {
+    countStrike += 1
+    checkMe()
+    niv = niv - 1;
+  }
+  sum = 0;
+  if ((l == k) && (wi != 1)) {
     for (s = 0; s < n; s++) {
-      sum = sum + martixArr[s][k];
+      sum = sum + martixArr[s][s];
     }
     if (sum == 0) {
       countStrike += 1
       checkMe()
       niv = niv - 1;
+      wi = 1;
     }
     sum = 0;
-    if ((l == k) && (wi != 1)) {
-      for (s = 0; s < n; s++) {
-      sum = sum + martixArr[s][s];
-      }
-      if (sum == 0) {
-        countStrike += 1
-        checkMe()
-        niv = niv - 1;
-      wi = 1;
-      }
-      sum = 0;
+  }
+  if (li != 1) {
+    f = 0;
+    for (s = 4; s >= 0; s--) {
+      sum = sum + martixArr[s][f];
+      f = f + 1;
     }
-    if (li != 1) {
-      f = 0;
-      for (s = 4; s >= 0; s--) {
-        sum = sum + martixArr[s][f];
-        f = f + 1;
-      }
-      if (sum == 0) {
-        countStrike += 1
-        checkMe()
-        niv = niv - 1;
-        li = 1;
-      }
-      sum = 0;
+    if (sum == 0) {
+      countStrike += 1
+      checkMe()
+      niv = niv - 1;
+      li = 1;
     }
-  
+    sum = 0;
+  }
+
   if (niv === 0) {
     $('#footer, #lead').fadeOut()
     $('#winBg').text("Bingo Go...!").addClass('leads')
     to_speak("bingo go..!")
-    socket.emit("uwin", {name: playerName, room: x})
+    socket.emit("uwin", { name: playerName, room: x })
   }
-  
+
 }
 
+// to speak
 function to_speak(texts) {
   speech.text = texts
   window.speechSynthesis.speak(speech);
 }
 
+// show name
 function showName(array) {
   showPlayer.innerHTML = ""
   dummyList = []
   array.forEach(element => {
     const spanName = document.createElement('span')
     dummyList.push(element.clientName)
-      if (serverName === element.clientName) {
-        spanName.setAttribute('class', 'active')
-        spanName.innerText = element.clientName;
+    if (serverName === element.clientName) {
+      spanName.setAttribute('class', 'active')
+      spanName.innerText = element.clientName;
     }
     if (serverName === playerName) {
-    document.getElementById("play-area").classList.remove("dim")
-  }
+      document.getElementById("play-area").classList.remove("dim")
+    }
     spanName.innerText = element.clientName;
     showPlayer.appendChild(spanName)
   });
